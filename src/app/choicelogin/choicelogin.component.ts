12f0da099ed59e73cf01916b3d5bee62d54707b0
Mifist <daria1992moskalets@gmail.com>
2017-04-18 17:31:49 +0300

    Добавлен выбор кого регистрировать

---
+++ src/app/choicelogin/choicelogin.component.ts
@@ -0,0 +1,70 @@
+﻿import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { AlertService, UserService } from '../_services/index';
+
+@Component({
+    moduleId: module.id,
+    templateUrl: 'choicelogin.component.html',
+    styleUrls: ['choicelogin.component.css']
+})
+
+export class ChoiceloginComponent {
+    model: any = {};
+    loading = false;
+    formowns = ['Форма собственности 11', 'Форма собственности 22', 'Форма собственности 33'];
+
+
+    constructor(
+        private router: Router,
+        private userService: UserService,
+        private alertService: AlertService) { }
+
+    blurel(el: any) {
+        //alert(el.target.value.trim()+"1");
+        //console.log(this.model);
+        console.log(this.model);
+        //console.log(f.value); 
+        if (el.target.value.trim() == '') {
+            el.target.parentNode.classList.remove('input--filled');
+        }
+        else {
+            el.target.parentNode.classList.add('input--filled');
+        }
+
+    }
+
+    onKeyUp()
+    {
+        //console.log(this.model);
+    }
+
+
+    register() {
+        console.log(this.model);
+        //alert(1);
+        this.loading = true;
+
+        //this.userService.create(this.model);
+        //console.log(this.model);
+        //alert(0);
+
+        
+        console.log('----------');
+
+        this.userService.create(this.model)
+            .subscribe(
+            data => {
+                    console.log(this.model);
+                    console.log(data);
+
+                    this.alertService.success('Регистрация прошла успешно', true);
+                    this.router.navigate(['/login']);
+                },
+                error => {
+                    this.alertService.error(error._body);
+                    this.loading = false;
+                });
+        
+    }
+}
