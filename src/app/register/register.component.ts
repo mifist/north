ad3d85e4bb2e222fd355bb2ab7aea63cbfd03169
Mifist <daria1992moskalets@gmail.com>
2017-04-24 12:50:27 +0300

    Стилилизация ошибки ИНН

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -12,46 +12,76 @@ import { AlertService, UserService } from '../_services/index';
 export class RegisterComponent {
     model: any = {};
     loading = false;
-    formowns = ['Форма собственности 11', 'Форма собственности 22', 'Форма собственности 33'];
-
+    formowns = ['Форма собственности 1', 'Форма собственности 2', 'Форма собственности 3'];
+    errorINN = '';
 
     constructor(
         private router: Router,
         private userService: UserService,
-        private alertService: AlertService) { }
+        private alertService: AlertService) {
+    }
+    loadel(ss: string) {
+        /* alert(ss);
+         console.log((<HTMLInputElement>document.getElementById(ss)).value);*/
+        let yyy: HTMLInputElement;
+
+        if ((<HTMLInputElement>document.getElementById(ss)).value.trim() === '') {
+            /* el.parentElement.classList.remove('input--filled');*/
+            document.getElementById(ss).parentElement.classList.remove('input--filled');
+        }
+        else {
+            /* el.parentElement.classList.add('input--filled');*/
+            document.getElementById(ss).parentElement.classList.add('input--filled');
+        }
+
+    }
 
     blurel(el: any) {
-        //alert(el.target.value.trim()+"1");
-        //console.log(this.model);
         console.log(this.model);
-        //console.log(f.value); 
-        if (el.target.value.trim() == '') {
+        if (el.target.value.trim() === '') {
             el.target.parentNode.classList.remove('input--filled');
+            this.errorINN = '';
+            return;
         }
         else {
             el.target.parentNode.classList.add('input--filled');
         }
-
+        if (el.target.id == 'inputusername') {
+            console.log(el.target.value.length);
+            if (!this.checkINN(el.target.value)) {
+                this.errorINN = 'ИНН некорректный';
+                el.target.focus();
+            } else {
+                this.errorINN = '';
+            }
+        }
     }
 
-    onKeyUp()
-    {
-        //console.log(this.model);
+
+
+
+    checkINN(inputNumber: any) {
+       /* преобразуем в строку*/
+        inputNumber = "" + inputNumber;
+        /*преобразуем в массив*/
+        inputNumber = inputNumber.split('');
+        /*для ИНН в 10 знаков*/
+        if ((inputNumber.length == 10) && (inputNumber[9] == ((2 * inputNumber[0] + 4 * inputNumber[1] + 10 * inputNumber[2] + 3 * inputNumber[3] + 5 * inputNumber[4] + 9 * inputNumber[5] + 4 * inputNumber[6] + 6 * inputNumber[7] + 8 * inputNumber[8]) % 11) % 10)) {
+            return true;
+            /*для ИНН в 12 знаков*/
+        } else if ((inputNumber.length == 12) && ((inputNumber[10] == ((7 * inputNumber[0] + 2 * inputNumber[1] + 4 * inputNumber[2] + 10 * inputNumber[3] + 3 * inputNumber[4] + 5 * inputNumber[5] + 9 * inputNumber[6] + 4 * inputNumber[7] + 6 * inputNumber[8] + 8 * inputNumber[9]) % 11) % 10) && (inputNumber[11] == ((3 * inputNumber[0] + 7 * inputNumber[1] + 2 * inputNumber[2] + 4 * inputNumber[3] + 10 * inputNumber[4] + 3 * inputNumber[5] + 5 * inputNumber[6] + 9 * inputNumber[7] + 4 * inputNumber[8] + 6 * inputNumber[9] + 8 * inputNumber[10]) % 11) % 10))) {
+            return true;
+        } else {
+            return false;
+        }
     }
-
-
+    onKeyUp() {
+        /*console.log(this.model);*/
+    }
     register() {
         console.log(this.model);
-        //alert(1);
         this.loading = true;
-
-        //this.userService.create(this.model);
-        //console.log(this.model);
-        //alert(0);
-
-        
         console.log('----------');
-
         this.userService.create(this.model)
             .subscribe(
             data => {
@@ -65,6 +95,15 @@ export class RegisterComponent {
                     this.alertService.error(error._body);
                     this.loading = false;
                 });
-        
     }
+    keyupel(el) {
+        if (el.target.id === 'inputusername') {
+            if (!this.checkINN(el.target.value)) {
+                this.errorINN = 'ИНН некорректный';
+            } else {
+                this.errorINN = '';
+            }
+        }
+    }
+
 }
